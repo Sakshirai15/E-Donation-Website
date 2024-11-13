595d47ad60053afee1f3ac2a3f8c1c9abd84f570
Sakshirai15 <sakshi.fiver@gmail.com>
2024-11-13 22:09:23 +0530

    Designing the database schema and creating a mysql table.

--- schema.sql
+++ schema.sql
@@ -1,24 +1,22 @@
-CREATE DATABASE E_donation_website; 
+create database Edonationwebsite;
+use EDonationwebsite;
 
-USE E_donation_website;
-  
---1. Users Table
 CREATE TABLE Users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
     email VARCHAR(100) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL,
-    role ENUM('donor', 'recipient', 'admin') DEFAULT 'donor',
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP);
+    role ENUM('donor', 'recipient', 'admin') DEFAULT 'donor');
 
--- 2. Categories Table
-CREATE TABLE Categories (
-    category_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL UNIQUE,
-    description TEXT);
+CREATE TABLE Products (
+    product_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    type ENUM('new', 'like new', 'used', 'for parts') NOT NULL,
+    status ENUM('available', 'pending', 'donated') DEFAULT 'available',
+    FOREIGN KEY (user_id) REFERENCES Users(user_id));
 
--- 3. DonationRequests Table
 CREATE TABLE DonationRequests (
     request_id INT AUTO_INCREMENT PRIMARY KEY,
     product_id INT NOT NULL,
@@ -28,15 +26,13 @@ CREATE TABLE DonationRequests (
     FOREIGN KEY (product_id) REFERENCES Products(product_id) ON DELETE CASCADE,
     FOREIGN KEY (recipient_id) REFERENCES Users(user_id) ON DELETE CASCADE);
 
--- 4. Donations Table
 CREATE TABLE Donations (
     donation_id INT AUTO_INCREMENT PRIMARY KEY,
-    product_id INT NOT NULL,
-    donor_id INT NOT NULL,
-    recipient_id INT NOT NULL,
+    product_id INT,
+    donor_id INT,
+    recipient_id INT,
     donated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (product_id) REFERENCES Products(product_id) ON DELETE CASCADE,
     FOREIGN KEY (donor_id) REFERENCES Users(user_id) ON DELETE SET NULL,
     FOREIGN KEY (recipient_id) REFERENCES Users(user_id) ON DELETE SET NULL);
-
 
